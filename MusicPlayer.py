ec647154a6711e2b34342339e06a90cdeb8f2aa2
Ben Willems <ben.dan.willems@gmail.com>
2019-07-24 15:42:47 +0100

    Music Player unfished: File directory works

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -1,8 +1,30 @@
 import os
+from playsound import playsound
 
-musicList = ()
+musicList = []
+musicDirectory = './Music/'
 
 
 def play(choice):
-    if musicList.
-    return os.open(musicList[choice])
+    playsound(musicDirectory + musicList[choice])
+
+
+def creatFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print('Error: Creating directory. ' + directory)
+
+
+def checkMusic():
+    for r, d, f in os.walk(musicDirectory):
+        for file in f:
+            if file.endswith('.mp3'):
+                musicList.append(file)
+
+
+creatFolder(musicDirectory)
+checkMusic()
+print(musicList)
+play(0)
