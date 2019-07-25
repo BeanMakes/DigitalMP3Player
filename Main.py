9e88845029963dd0e5b3413ac5be8857b2de5e79
Ben Willems <ben.dan.willems@gmail.com>
2019-07-25 11:04:10 +0100

    Music Player Unfinished: Basic GUI is made with a play and stop button. Refactored MusicPlayer class

--- Main.py
+++ Main.py
@@ -1,25 +1,38 @@
-import MusicPlayer as mp
+from MusicPlayer import MusicPlayer
 import wx
 
 
+mp = MusicPlayer()
+
+
 class mainWindow(wx.Frame):
+
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200, -1))
         self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
         panel = wx.Panel(self, wx.ID_ANY)
         button = wx.Button(panel, wx.ID_ANY, 'Play', (10, 10))
-        button.Bind(wx.EVT_BUTTON, onButton)
+        button.Bind(wx.EVT_BUTTON, onPlayButton)
+
+        buttonS = wx.Button(panel, wx.ID_ANY, 'Stop', (10, 50))
+        buttonS.Bind(wx.EVT_BUTTON, onStopButton)
 
         self.Show()
 
 
-def onButton(event):
-    mp.playMusic(0)
+def onPlayButton(event):
+    mp.checkMusic()
+    mp.setChoice = 0
+    mp.playMusic()
+
+
+def onStopButton(event):
+    mp.stopMusic()
 
 
 app = wx.App(False)
-frame = mainWindow(None, "Hello World")
+frame = mainWindow(None, "Music Player")
 
 
 app.MainLoop()
--- MusicPlayer.py
+++ MusicPlayer.py
@@ -2,39 +2,41 @@ import os
 import simpleaudio as sa
 
 
-musicList = []
-musicDirectory = './Music/'
-wav_obj = object
-musicIsPlaying = False
+class MusicPlayer:
+    def __init__(self):
+        self.__musicList = []
+        self.__musicDirectory = './Music/'
+        self.__wav_obj = object
+        self.__musicChoice = 0
+
+    def playMusic(self):
+        sa.stop_all()
+        self.__wav_obj = sa.WaveObject.from_wave_file(self.__musicDirectory + self.__musicList[self.__musicChoice])
+        play_obj = self.__wav_obj.play()
+
+    def stopMusic(self):
+        sa.stop_all()
+
+    def nextMusic(self):
+        sa.stop_all()
+        self.__wav_obj = sa.WaveObject.from_wave_file(self.__musicDirectory + self.__musicList[musicChoice+1])
+        play_obj = wav_obj.play()
+
+    def checkMusic(self):
+        for r, d, f in os.walk(self.__musicDirectory):
+            for file in f:
+                if file.endswith('.wav'):
+                    self.__musicList.append(file)
+
+    def setChoice(self, choice):
+        self.__musicChoice = choice
 
 
-def playMusic(choice):
-    wav_obj = sa.WaveObject.from_wave_file(musicDirectory + musicList[choice])
-    play_obj = wav_obj.play()
-    musicPlaying = play_obj.is_playing()
-    play_obj.wait_done()
+    def getChoice(self):
+        return self.__musicChoice
+
+    musicChoice = property(getChoice,setChoice)
 
 
-def creatFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print('Error: Creating directory. ' + directory)
-
-
-def checkMusic():
-    for r, d, f in os.walk(musicDirectory):
-        for file in f:
-            if file.endswith('.wav'):
-                musicList.append(file)
 
     #musicList = [file for r, d, f in os.walk(musicDirectory) for file in f if file.endswith('.mp3')]
-
-
-def isMusicRunning():
-    return musicIsPlaying
-
-
-creatFolder(musicDirectory)
-checkMusic()
