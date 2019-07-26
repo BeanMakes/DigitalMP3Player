19402bc4a4cbff743cc650ba1acdea17c7063fa3
Ben Willems <ben.dan.willems@gmail.com>
2019-07-26 17:17:49 +0100

    Added listbox

--- Main.py
+++ Main.py
@@ -11,18 +11,24 @@ class mainWindow(wx.Frame):
         self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
         panel = wx.Panel(self, wx.ID_ANY)
+
+        #Button for play
         button = wx.Button(panel, wx.ID_ANY, 'Play', (100, 100))
         button.Bind(wx.EVT_BUTTON, onPlayButton)
 
+        #Button for stop
         buttonS = wx.Button(panel, wx.ID_ANY, 'Stop', (100, 150))
         buttonS.Bind(wx.EVT_BUTTON, onStopButton)
 
+        #Button for next
         buttonN = wx.Button(panel, wx.ID_ANY, 'Next', (190, 100))
         buttonN.Bind(wx.EVT_BUTTON, onNextButton)
 
+        #Button for previous
         buttonP = wx.Button(panel, wx.ID_ANY, 'Previous', (10, 100))
         buttonP.Bind(wx.EVT_BUTTON, onPrevButton)
 
+        #Creating listbox for GUI
         self.text = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
         listBox = wx.ListBox(panel, pos = (40,10), size = (200,70), choices = mp.getMusicList(),style = wx.LB_SINGLE)
         listBox.SetScrollPos(wx.HORIZONTAL, listBox.GetScrollRange(wx.HORIZONTAL), refresh=True)
